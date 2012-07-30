be6341fe0cbfc7b996a0c75c05134a47d06a83f0
AITI Student <aiti-student@AITI-87.(none)>
2012-07-30 16:41:05 +0000

    update

--- hostels/urls.py
+++ hostels/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
     url(r'^homepage/$', 'hostels.views.frontpage'),
     url(r'^homepage/(?P<id>\d+)/displayhostels/$', 'hostels.views.hostels_list'),
     url(r'^homepage/displayhostels/(?P<id>\d+)/$','hostels.views.hostels_detail'),
+    url(r'^homepage/displayhostels/(?P<id>\d+)/$','hostels.views.studregister'),
+    
     #url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?/studreg/$','hostels.views.studregister'),
     #url(r'^homepage/displayhostels/(?P<id>\d+)/((?P<hostelinfo>.*)/)?/studreg/studcon$','hostels.views.studconfirm'),
 
